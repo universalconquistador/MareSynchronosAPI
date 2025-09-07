27e70c6c0c335a056ab3c7c5fd7f497b016d476d
Universal Conquistador <universalconquistador@gmail.com>
2025-09-07 16:14:54 -0700

    Initial Syncshell broadcast API.

---
+++ MareSynchronosAPI/Dto/Group/BroadcastSendDto.cs
@@ -0,0 +1,18 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MareSynchronos.API.Dto.Group;
+
+/// <summary>
+/// Information about a group that the player wants to broadcast.
+/// </summary>
+/// <param name="BroadcastGroupId">The group's ID (GID).</param>
+[MessagePackObject(keyAsPropertyName: true)]
+public record BroadcastSendDto(string BroadcastGroupId)
+{
+
+}
--- MareSynchronosAPI/SignalR/IMareHubClient.cs
+++ MareSynchronosAPI/SignalR/IMareHubClient.cs
@@ -60,4 +60,6 @@ public interface IMareHubClient : IMareHub
     void OnGposeLobbyPushCharacterData(Action<CharaDataDownloadDto> act);
     void OnGposeLobbyPushPoseData(Action<UserData, PoseData> act);
     void OnGposeLobbyPushWorldData(Action<UserData, WorldData> act);
+
+    void OnBroadcastListeningChanged(Action<bool> act);
 }
