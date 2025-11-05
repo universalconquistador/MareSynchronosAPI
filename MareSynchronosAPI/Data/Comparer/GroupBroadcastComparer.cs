a080bf351eb3c5e14b88bd9d3bb29e41b67496a7
Suterusu Kusanagi <suterusu@suterusu.com>
2025-11-05 17:40:07 -0500

    Added comparison for broadcast group messages to reduce timed UI flicker/ImGui refresh

---
+++ MareSynchronosAPI/Data/Comparer/GroupBroadcastComparer.cs
@@ -0,0 +1,44 @@
+﻿using MareSynchronos.API.Dto.Group;
+
+namespace MareSynchronos.API.Data.Comparer
+{
+    public class GroupBroadcastComparer : IEqualityComparer<GroupBroadcastDto>
+    {
+        private static readonly GroupBroadcastComparer _instance = new();
+
+        public static GroupBroadcastComparer Instance => _instance;
+
+        public bool Equals(GroupBroadcastDto? x, GroupBroadcastDto? y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (x is null || y is null) return false;
+
+            if (!Equals(x.Group, y.Group)) return false;
+            if (!Equals(x.Owner, y.Owner)) return false;
+
+            if (x.CurrentMemberCount != y.CurrentMemberCount) return false;
+            if (x.Passwordless != y.Passwordless) return false;
+            if (x.IsGuestModeEnabled != y.IsGuestModeEnabled) return false;
+
+            var xb = x.Broadcasters;
+            var yb = y.Broadcasters;
+            if (xb == null || yb == null) return xb == yb;
+
+            return xb.SequenceEqual(yb);
+        }
+
+        public int GetHashCode(GroupBroadcastDto obj)
+        {
+            var hc = new HashCode();
+            hc.Add(obj.Group);
+            hc.Add(obj.Owner);
+            hc.Add(obj.CurrentMemberCount);
+            hc.Add(obj.Passwordless);
+            hc.Add(obj.IsGuestModeEnabled);
+            if (obj.Broadcasters != null)
+                foreach (var b in obj.Broadcasters) hc.Add(b);
+            return hc.ToHashCode();
+        }
+    }
+
+}
