17855468da9df4e9a51f756ab6b5d047188ed1df
Suterusu Kusanagi <suterusu@suterusu.com>
2026-02-08 13:33:28 -0500

    Update Pair Request Dto for multiple request types

--- MareSynchronosAPI/Dto/User/UserPairRequestDto.cs
+++ MareSynchronosAPI/Dto/User/UserPairRequestDto.cs
@@ -4,8 +4,8 @@ using MessagePack;
 namespace MareSynchronos.API.Dto.User;
 
 [MessagePackObject(keyAsPropertyName: true)]
-public record UserPairRequestDto(string RequestTargetIdent);
-
+public record UserPairRequestDto(string? RequestTargetIdent = null, UserData? UserData = null);
+ 
 [MessagePackObject(keyAsPropertyName: true)]
 public record UserPairRequestFullDto(UserData Requestor, string RequestorIdent, UserData RequestTarget, string RequestTargetIdent);
 
