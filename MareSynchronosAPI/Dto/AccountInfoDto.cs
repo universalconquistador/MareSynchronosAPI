177e226ae213efd329d834162e818122681dffc4
Suterusu Kusanagi <suterusu@suterusu.com>
2026-03-14 21:19:54 -0400

    Update Alias

---
+++ MareSynchronosAPI/Dto/AccountInfoDto.cs
@@ -0,0 +1,11 @@
+﻿using MareSynchronos.API.Data;
+using MessagePack;
+
+namespace MareSynchronos.API.Dto;
+
+[MessagePackObject(keyAsPropertyName: true)]
+public record AccountInfoDto
+{
+    public List<UserData> AccountUids {get; set;} = new();
+    public List<GroupData> AccountGroups {get; set;} = new();
+}
