0a61a5866019eb550dab2375a7803f4f325f60b3
Suterusu Kusanagi <suterusu@suterusu.com>
2026-03-15 00:21:40 -0400

    Move alias validation to API

---
+++ MareSynchronosAPI/Data/Validators/AliasValidator.cs
@@ -0,0 +1,15 @@
+﻿
+namespace MareSynchronos.API.Data.Validators
+{
+    public static class AliasValidator
+    {
+        public static bool IsValidAlias(string alias)
+        {
+            if (string.IsNullOrEmpty(alias)) return false;
+            foreach (char chara in alias)
+                if (!(char.IsLetterOrDigit(chara) || chara == '_' || chara == '-'))
+                    return false;
+            return true;
+        }
+    }
+}
