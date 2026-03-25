a04874002e6405d317595d7e07d024807fd32399
Suterusu Kusanagi <suterusu@suterusu.com>
2026-03-24 21:15:07 -0400

    Allow for null or whitespace

--- MareSynchronosAPI/Data/Validators/AliasValidator.cs
+++ MareSynchronosAPI/Data/Validators/AliasValidator.cs
@@ -3,9 +3,9 @@ namespace MareSynchronos.API.Data.Validators
 {
     public static class AliasValidator
     {
-        public static bool IsValidAlias(string alias)
+        public static bool IsValidAlias(string? alias)
         {
-            if (string.IsNullOrEmpty(alias)) return false;
+            if (string.IsNullOrWhiteSpace(alias)) return true;
             if (alias.Length < 5 || alias.Length > 15) return false;
             foreach (char chara in alias)
                 if (!(char.IsLetterOrDigit(chara) || chara == '_' || chara == '-'))
