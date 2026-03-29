4f4cf550ecc1c287194a5aad35b561cac82e86a1
Suterusu Kusanagi <suterusu@suterusu.com>
2026-03-29 18:42:55 -0400

    Move profile check to OnlineUserIdentDto

--- MareSynchronosAPI/Data/UserData.cs
+++ MareSynchronosAPI/Data/UserData.cs
@@ -5,8 +5,6 @@ namespace MareSynchronos.API.Data;
 [MessagePackObject(keyAsPropertyName: true)]
 public record UserData(string UID, string? Alias = null)
 {
-    public bool? HasProfile { get; set; }
-
     [IgnoreMember]
     public string AliasOrUID => string.IsNullOrWhiteSpace(Alias) ? UID : Alias;
 }
