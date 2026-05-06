6512e2bf85715ac73947ee709bdc2e7a97c5ece7
Suterusu Kusanagi <suterusu@suterusu.com>
2026-05-06 18:50:48 -0400

    Change for address Dto

--- MareSynchronosAPI/Data/AdditionalTypes/LifestreamParseableAddress.cs
+++ MareSynchronosAPI/Data/AdditionalTypes/LifestreamParseableAddress.cs
@@ -1,4 +1,17 @@
 ﻿
 namespace MareSynchronos.API.Data.AdditionalTypes;
 
-public record LifestreamParseableAddress((string Name, int World, int City, int Ward, int PropertyType, int Plot, int Apartment, bool ApartmentSubdivision, bool AliasEnabled, string Alias) AddressBookEntry);
+public record LifestreamParseableAddress(AddressBookEntryDto AddressBookEntry);
+
+public record AddressBookEntryDto(
+    string Name,
+    int World,
+    int City,
+    int Ward,
+    int PropertyType,
+    int Plot,
+    int Apartment,
+    bool ApartmentSubdivision,
+    bool AliasEnabled,
+    string Alias
+    );
